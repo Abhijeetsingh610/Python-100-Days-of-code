205c932406edd7cde2231b518b1433f898477a1c
Abhijeetsingh <1032211123@mitwpu.edu.in>
2023-02-27 21:58:02 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,12 @@
+print("wellcome to the Dog band name generator.")
+
+city = input("Which city did you grow up in ?")
+
+print(city)
+
+pet = input("What is the name  of a pet?")
+
+print("Your band name could be " + city + " " + pet)
+
+
+'''**************Concatication of words******************'''
